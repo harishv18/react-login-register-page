3f443af0c6077e09fa0f2f34f536a8c8ff3f04c2
Harish V <harish00411@gmail.com>
2023-07-03 01:46:28 +0530

    added harish

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function HomePage() {
     return (
         <div className="text-center">
-            <h1 className="main-title home-page-title">welcome to our app</h1>
+            <h1 className="main-title home-page-title">welcome to harish</h1>
             <Link to="/">
                 <button className="primary-button">Log out</button>
             </Link>
